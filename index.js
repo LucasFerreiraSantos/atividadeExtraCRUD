9025430e10a43efb4c09e23fc0cc17d7bd5752dd
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-13 23:10:22 -0300

    feita a função para listar as tarefas

--- index.js
+++ index.js
@@ -3,16 +3,13 @@
 //     - A função deve verificar se o título da tarefa é fornecido. Se não for, deve exibir uma mensagem de erro indicando que o título é obrigatório.
 //     - A nova tarefa deve ser um objeto com as propriedades: **`title`**, **`description`** e **`completed`** (inicializado como **`false`**).
 const arrayTask = [];
-const title = prompt("Qual o título da tarefa? ");
-const description = prompt("Qual a descrição da tarefa? ");
+const title = "Café da manhã"; //aqui vai o prompt depois
+const description = "Fazer um café da manhã, às 7h da matina."; //aqui vai outro prompt
 let completed = false;
-
-// percorrer os indices do array
-const indexTasks = arrayTask.findIndex((tasks) => tasks.title);
 
 function createTask(title, description) {
   if (title === "") {
-    alert("O título da tarefa é obrigatório.");
+    console.log("O título da tarefa é obrigatório.");
   } else {
     arrayTask.push({
       title,
@@ -28,17 +25,19 @@ createTask(title, description);
 //     - Crie uma função chamada **`listTasks()`** que exiba uma lista formatada de todas as tarefas cadastradas.
 //     - Percorra o array de tarefas e mostre o índice, título, descrição e o status (concluída ou não) de cada tarefa.
 function listTasks() {
-  if (completed) {
-    completed = "Tarefa concluída!";
-  } else {
-    completed = "Tarefa NÃO concluída!";
-  }
-  arrayTask.forEach((tasks) => {
+  arrayTask.forEach((tasks, index) => {
+    if (!completed) {
+      tasks.completed = "Tarefa NÃO concluída!";
+    } else {
+      tasks.completed = "Tarefa concluída!";
+    }
     console.log(
-      `${tasks.indexTasks} | ${tasks.title} | ${tasks.description} | ${tasks.completed}`
+      `${index} | ${tasks.title} | ${tasks.description} | ${tasks.completed}`
     );
   });
 }
+
+listTasks();
 
 // 3. **Atualização de Status de Tarefa:**
 //     - Crie uma função chamada **`updateTaskStatus(index, completed)`** que permita atualizar o status de conclusão de uma tarefa.
